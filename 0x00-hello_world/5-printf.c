57feaec2b76b2fab8ed47948db89b8452edb91fb
Chidera6 <dictasteil@gmail.com>
2021-08-28 12:54:04 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - main function
+*
+*
+*Return: return 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is apiece of art,");
+return (0);
+}
+
+
+
+
+
