7d8a9e4194adf55dc5f83b098c2503fb83adb689
Chidera6 <dictasteil@gmail.com>
2021-09-22 12:01:10 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,26 +1,20 @@
 #include "main.h"
 
 /**
- * _strncpy - Copies at most an inputted number
- *            of bytes from string src into dest.
- * @dest: The buffer storing the string copy.
- * @src: The source string.
- * @n: The maximum number of bytes to copied from src.
- *
- * Return: A pointer to the resulting string dest.
+ * _strncpy - copies a string
+ * @dest: copy destination
+ * @src: copy source
+ * @n: bytes to be copied
+ * Return: copied string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, src_len = 0;
+	char *ptr = dest;
 
-	while (src[index++])
-		src_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
-
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
-
-	return (dest);
+	while (*src && n--)
+		*dest++ = *src++;
+	if (!*src)
+		while (*dest && n--)
+			*dest++ = 0;
+	return (ptr);
 }
